ee8f15346e590826fb3bb4c0e549e0c9214d0bff
Humberto <humberto.angel.duran@gmail.com>
2025-02-09 12:25:08 -0600

    Refactor parameter handling and update main logic for encryption process

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,33 @@
 #include <string>
 #include <unordered_map>
 #include <sstream>
+#include <vector>
 
 using namespace std;
+
+struct Parameters
+{
+    string fasta;
+    string text;
+    unsigned int offset;
+    unsigned int tries;
+
+    Parameters(string fasta, unsigned int offset, unsigned int tries, string text)
+    {
+        this->fasta = fasta;
+        this->offset = offset;
+        this->offset = offset;
+        this->text = text;
+    }
+
+    Parameters()
+    {
+        fasta = "";
+        offset = 0;
+        tries = 0;
+        text = "";
+    }
+};
 
 void CleanText(string &text)
 {
@@ -28,7 +53,7 @@ void CleanText(string &text)
     }
 }
 
-void SetParameters(string text, string &fasta, unsigned int &offset, unsigned int &tries)
+void GetParameters(string text, Parameters *p)
 {
     if (!text.empty() && text[0] == '#')
         text.erase(0, 1); // Remove first character if it's '#'
@@ -41,23 +66,24 @@ void SetParameters(string text, string &fasta, unsigned int &offset, unsigned in
     {
         if (pos == 1)
         {
-            offset = stoi(token);
+            p->offset = stoi(token);
         }
         else if (pos == 2)
         {
-            tries = stoi(token);
+            p->tries = stoi(token);
         }
         else
         {
-            fasta = token;
+            p->fasta = "../instructions/" + token;
         }
 
         pos++;
     }
 }
 
-void Encrypt(const string &filePath)
+void SetParametersVector(vector<Parameters *> *p)
 {
+    const string filePath = "../encrypt/Original_document.txt";
     cout << "Reading file..." << endl;
     ifstream file(filePath);
     if (!file.is_open())
@@ -66,33 +92,104 @@ void Encrypt(const string &filePath)
         return;
     }
 
-    string line, rule, text, fasta;
-    unsigned int offset = 0, tries = 0;
-    unordered_map<char, char> protein_dict = {{'A', '\0'}, {'C', '\0'}, {'D', '\0'}, {'E', '\0'}, {'F', '\0'}, {'G', '\0'}, {'H', '\0'}, {'I', '\0'}, {'K', '\0'}, {'L', '\0'}, {'M', '\0'}, {'N', '\0'}, {'P', '\0'}, {'Q', '\0'}, {'R', '\0'}, {'S', '\0'}, {'T', '\0'}, {'V', '\0'}, {'W', '\0'}, {'Y', '\0'}, {'B', 'Z'}, {'Z', 'B'}, {'X', 'J'}, {'J', 'X'}, {'U', 'O'}, {'O', 'U'}};
+    // Variables
+    string line, rule, text;
+
+    // Pointer to a Parameters object
+    Parameters *params;
 
     while (getline(file, line))
     {
         if (line[0] == '#')
         {
+            params = new Parameters();
             rule = line;
             // Extract the parameters
-            SetParameters(rule, fasta, offset, tries);
+            GetParameters(rule, params);
             // Set text to an empty string again
+            p->push_back(params);
+            // Clear the text
             text = "";
         }
-        text += line;
-        // Clean text
-        CleanText(text);
-        // Update the vocabulary only when there is a clean text;
+        else
+        {
+            text += line;
+            // Clean text
+            CleanText(text);
+            // Add the text to the parameters
+            params->text += text;
+        }
     }
 
     file.close();
 }
 
+void CreateVocabulary(Parameters *p)
+{
+    ifstream fasta(p->fasta);
+
+    if (!fasta.is_open())
+        return;
+
+    unordered_map<char, char> protein_dict = {{'A', '\0'}, {'C', '\0'}, {'D', '\0'}, {'E', '\0'}, {'F', '\0'}, {'G', '\0'}, {'H', '\0'}, {'I', '\0'}, {'K', '\0'}, {'L', '\0'}, {'M', '\0'}, {'N', '\0'}, {'P', '\0'}, {'Q', '\0'}, {'R', '\0'}, {'S', '\0'}, {'T', '\0'}, {'V', '\0'}, {'W', '\0'}, {'Y', '\0'}, {'B', 'Z'}, {'Z', 'B'}, {'X', 'J'}, {'J', 'X'}, {'U', 'O'}, {'O', 'U'}};
+    string line;
+
+    int offset = p->offset;
+    unsigned int ascii_value = 0;
+    bool first = true;
+
+    while (getline(fasta, line))
+    {
+        if (offset > line.size())
+        {
+            offset -= line.size();
+            continue;
+        }
+
+        char letter = line[offset];
+        // get the ascii value of the letter
+        ascii_value = (int)letter;
+        // for each letter in the abecedary, only capital letters
+        for (char c = 'A'; c <= 'Z'; c++)
+        {
+            // except for the letters B, X, U, Z, J, O
+            if (c == 'B' || c == 'X' || c == 'U' || c == 'Z' || c == 'J' || c == 'O')
+                continue;
+
+            // if the ascii value of the letter is the same as the ascii value of the current letter break the loop
+            if (ascii_value == (int)c)
+                break;
+
+            // if the letter already has a value in the dictionary, continue
+            if (protein_dict[c] != '\0')
+                continue;
+
+            // add the value to the dictionary
+            protein_dict[c] = letter;
+            break;
+        }
+
+        if (first)
+        {
+            offset = ascii_value;
+            first = false;
+        }
+    }
+
+    fasta.close();
+}
+
+void Encrypt()
+{
+
+    // vector of parameter pointers
+    vector<Parameters *> parameters;
+    // Sets the vector with the parameters
+    SetParametersVector(&parameters);
+}
+
 int main()
 {
-    const string filePath = "../encrypt/Original_document.txt";
-
-    Encrypt(filePath);
+    Encrypt();
     return 0;
 }
